b93a2893bb296bb5cc0deee21d5bdbce70ae78b3
aslah <aslahcholasseri@gmail.com>
2024-05-23 21:22:26 +0530

    axios

--- client/src/axios/axios.tsx
+++ client/src/axios/axios.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://aslah.online/api"; // Set your base URL here
+const baseURL = "https://aslah.online/api"; // Set your base URL here
 const axiosInstance = axios.create({ baseURL });
 
 // Add a request interceptor to include the JWT token in the headers
